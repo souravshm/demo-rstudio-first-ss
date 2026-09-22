75023ec1d1d2488419f3cc56e4c3f4533b5111eb
souravshm <sourav27@student.ubc.ca>
2026-09-22 10:16:53 -0700

    added comments

--- ReadME.md
+++ ReadME.md
@@ -3,3 +3,7 @@
 this is a demo for stat545
 
 this is an edit i made on github
+
+this is an edit I made on R studio
+
+another edit on R studio
--- test01.R
+++ test01.R
@@ -1,5 +1,14 @@
+#view the data first
+
 head(mtcars)
+
+#calculate the mean or avg.
 
 mean_mpg <- mean(mtcars$mpg)
 
+
+#view the mean
+
 mean_mpg
+
+#this is an edit made on Rstudio
